862e9d8d0c08fa4dce2c9eebb971c617a6af1ea3
Mika Hally <mika.hally@code.berlin>
2020-05-27 21:27:56 +0200

    Changed hero text

--- src/components/Hero4/Hero4.jsx
+++ src/components/Hero4/Hero4.jsx
@@ -7,14 +7,18 @@ class Hero4 extends Component {
         return (
             <div className="container hero4-wrapper">
                 <img className="hero-bg-color" src={require('../../assets/images/hero-bg.svg')} alt="hero-bg"/>
-                <img className="hero-bg-color-responsive" src={require('../../assets/images/hero-bg-responsive.svg')} alt="hero-bg"/>
+                <img className="hero-bg-color-responsive" src={require('../../assets/images/hero-bg-responsive.svg')}
+                     alt="hero-bg"/>
                 <div className="hero4-left">
-                    <h1>Your digital career starts here</h1>
+                    <h1>Your digital career starts here.</h1>
                     <p>
-                        Gain the skills, work experience, and industry relationships you need to kick off your career. Entirely remote & always free.
+                        Learn from digital experts and get access to everything for kick-starting your digital career.
+                        Entirely remote.
                     </p>
                     <div className="hero4-btn-wrapper">
-                        <a href="mailto:henrik.bredenbals@code.berlin"><button className="primary-btn">Get in touch</button></a>
+                        <a href="mailto:henrik.bredenbals@code.berlin">
+                            <button className="primary-btn">Get in touch</button>
+                        </a>
                     </div>
                 </div>
                 <div className="hero4-right">
